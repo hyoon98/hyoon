9e09bd429383e9a35279601d64d3d52bd13cc0d6
hyoon1998 <hyoon1998@gmail.com>
2022-02-23 02:22:16 -0800

    setup basic css for card

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Blog.css'
+import Card from '../Card/Card'
+import examplecard from '../../assets/examplecard.jpg'
 
 export default function Blog() {
   return (
-    <div className='Blog'>Blog</div>
+    <div className='Blog'>
+      <Card cover={examplecard} title="Test" desc="Just testing"/>
+    </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Card.css'
 import { useNavigate } from 'react-router-dom'
 
-export default function Card({cover, title, description, link}) {
+export default function Card({cover, title, desc, link}) {
     let navigate=useNavigate();
   return (
     <div className='Card' onClick={()=>{
@@ -10,7 +10,7 @@ export default function Card({cover, title, description, link}) {
     }}>
         <img className='cover' src={cover}/>
         <h2 className='card-title'>{title}</h2>
-        <p className='description'>{description}</p>
+        <p className='description'>{desc}</p>
     </div>
   )
 }
