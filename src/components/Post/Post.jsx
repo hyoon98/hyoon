6a300b02d69a4c99fb555088787b946c529fa339
hya62 <hansoo_yoon@sfu.ca>
2022-02-24 12:38:09 -0800

    added css to post page and rich text

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Post.css"
 import {useParams} from 'react-router-dom'
 import { gql,useQuery } from '@apollo/client'
+import {RichText} from '@graphcms/rich-text-react-renderer'
 
 function Post() {
     let {slug}=useParams();
@@ -9,17 +10,31 @@ function Post() {
         query{
             blogPost(where: {slug: "${slug}"}) {
             id
-            title
-            text
+            coverImage{
+              url
             }
+            title
+            text{
+              raw
+            }
+            date
+          }
         }
     `
     const {loading,error,data}=useQuery(GET_POST)
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
-
   return (
-    <div className='Post'>{data.blogPost.title}</div>
+    <div className='Post'>
+      <h1 className='post-title'>
+        {data.blogPost.title}
+      </h1>
+      <h4 className='post-date'>Published {data.blogPost.date}</h4>
+      <img className='post-img' src={data.blogPost.coverImage.url} alt="" />
+      <div className="post-content">
+        <RichText className="post-content" content={data.blogPost.text.raw}/>
+      </div>
+    </div>
   )
 }
 
