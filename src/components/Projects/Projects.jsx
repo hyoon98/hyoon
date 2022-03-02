8ae690ed57da20f38cf94652d423c5aeed91770c
hya62 <hansoo_yoon@sfu.ca>
2022-03-02 10:02:05 -0800

    setup projects graphcms

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,8 +3,24 @@ import Card from './Card/Card.js'
 import debttrackerimg from '../../assets/debttrackerimg.jpg'
 import cartoonimg from '../../assets/cartoon.jpg'
 import shoetrackerimg from '../../assets/shoetracker.png'
+import { useQuery } from '@apollo/client'
+
+const GET_PROJECTS=gql`
+  {
+  projects(orderBy: createdAt_DESC){
+    title
+    link
+    coverImage{
+      url
+    }
+  }
+}
+`
 
 export default function Projects() {
+    const {loading,error,data}=useQuery(GET_PROJECTS)
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
     return (
         <div className='Projects' id='projects'>
           <h3 className='project-title'>Projects</h3>
