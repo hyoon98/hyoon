0b0150e0006c1a7a6c6a72eb65c55ded617135cf
hya62 <hansoo_yoon@sfu.ca>
2022-03-02 10:23:52 -0800

    connect projects to graphcms

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,12 @@ import Card from './Card/Card.js'
 import debttrackerimg from '../../assets/debttrackerimg.jpg'
 import cartoonimg from '../../assets/cartoon.jpg'
 import shoetrackerimg from '../../assets/shoetracker.png'
-import { useQuery } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
 
 const GET_PROJECTS=gql`
   {
   projects(orderBy: createdAt_DESC){
+    id
     title
     link
     coverImage{
@@ -25,24 +26,12 @@ export default function Projects() {
         <div className='Projects' id='projects'>
           <h3 className='project-title'>Projects</h3>
           <div className="projects-container">
-            <Card
-              link="https://github.com/hyoon98/Debt-Tracker"
-              image={debttrackerimg}
-            >
-              Debt Tracker
-            </Card>
-            <Card
-              link="https://github.com/alhparsa/CSSS-Hackathon"
-              image={cartoonimg}
-            >
-              CSSS Hackathon
-            </Card>
-            <Card
-              link="https://github.com/hyoon98/Shoe-Tracker"
-              image={shoetrackerimg}
-            >
-              MEC Shoe Tracker
-            </Card>
+            {data.projects.map((project)=>(
+              <Card key={project.id} image={project.coverImage.url} link={project.link}>
+                {project.title}
+              </Card>
+              ))
+            }
           </div>
         </div>
     )
